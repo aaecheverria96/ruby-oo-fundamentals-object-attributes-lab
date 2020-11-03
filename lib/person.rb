04cd60ed7c07e2d860ba730d82df1a65450977f9
Your Username <you@example.com>
2020-11-03 12:26:01 -0500

    commit tree ff9432e8cb6cde4e1c6eb556609cd41bc021ff98

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+
+    def name=(name)
+         @name = name
+    end 
+
+    def name 
+        @name 
+    end 
+
+    def job=(this_persons_job) 
+        @job = this_persons_job 
+    end 
+
+    def job 
+        @job 
+    end 
+end 
